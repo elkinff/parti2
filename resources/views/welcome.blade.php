2f812000d00cd516f79bfba25546856f05350bcb
OscarSoler <oscarsoler25@gmail.com>
2018-01-11 08:57:34 -0500

    estructura de carpetas inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,10 @@
                 <div class="title m-b-md">
                     <h1>Hola</h1>
                     <h2>Hola</h2>
+                    <h3>Hola</h3>
+                    <h4>Hola</h4>
+                    <h5>Hola</h5>
+                    <h6>Hola</h6>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo

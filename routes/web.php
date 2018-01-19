6680faf43d72a15d7952bcb23165a9e08e4d91fa
elkinff <elkin9309@gmail.com>
2018-01-19 17:47:05 -0500

    arreglo ruta y finalizacion de integracion de apis de facebook y google

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
+Route::get('auth/{provider}', 'Auth\SocialAuthController@redirectToProvider')->name('social.auth');
+Route::get('auth/{provider}/callback', 'Auth\SocialAuthController@handleProviderCallback');
 
+Route::get('/', 'HomeController@index')->name('muro');
 
-Route::get('/login', function () {
-    return view('pages.auth.login');
-});
+// Route::get('/login', function () {
+//     return view('pages.auth.login');
+// });
 
-Route::get('/register', function () {
-    return view('pages.auth.register');
-});
+// Route::get('/register', function () {
+//     return view('pages.auth.register');
+// });
 
-Route::get('/recuperar', function () {
-    return view('pages.auth.recuperar');
-});
+// Route::get('/recuperar', function () {
+//     return view('pages.auth.recuperar');
+// });
 
 Route::get('/ejemplo', function () {
     return view('pages.dashboard.example');
@@ -24,11 +27,5 @@ Route::get('/email', function () {
 });
 
 
-
-Route::get('auth/{provider}', 'Auth\SocialAuthController@redirectToProvider')->name('social.auth');
-Route::get('auth/{provider}/callback', 'Auth\SocialAuthController@handleProviderCallback');
-
-
-Route::get('home', 'HomeController@index')->name('home');
 Auth::routes();
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');

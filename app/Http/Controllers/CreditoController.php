5247b468846825216ede33f57af4c83e211b542d
elkinff <elkin9309@gmail.com>
2018-02-23 18:03:00 -0500

    backend paginas confirmacion y respuesta credito agregar

--- app/Http/Controllers/CreditoController.php
+++ app/Http/Controllers/CreditoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class CreditoController extends Controller{
     
@@ -14,23 +15,36 @@ class CreditoController extends Controller{
     	return view('pages.dashboard.credito');
     }
 
-    public function confirmacionPasarela(){
-  //   	//Validar firma
-		// $signature = hash('sha256', $request->x_cust_id_cliente.'^86c18a3ad068b30d14c99a47940ad176bb0c7721^'.$request->x_ref_payco.'^'.$request->x_transaction_id.'^'.$request->x_amount.'^'.$request->x_currency_code);
-		
-		// if ($signature == $request->x_signature) {
-		// 	$codigoRespuesta = $request->x_cod_response;
-		// 	if ($codigoRespuesta == 1) {
-		// 		$usuario = User::findOrFail($request->extra1);
-		// 		// $usuario->saldo = $usuario->saldo + 
-		// 	}
-		// 	}else{
-		// 		$mensaje ="No Realizada, estado = ".$codigoRespuesta;
-		// 	}
-		// }else{
-		// 	$mensaje = "No coincide la firma";
-		// }
+    public function respuestaPasarela(Request $request){
+    	if ($request->x_franchise == "AM" || $request->x_franchise == "CR" || $request->x_franchise == "DC" || $request->x_franchise == "CR" || $request->x_franchise == "MC" $request->x_franchise == "SP") {
+    		alert()->info("Tu Pago se ha realizado satisfactoriamente, en unos segundos se reflejará en tu saldo");
+    		return view('pages.dashboard.credito');
+    	}else{
+    		alert()->info("Tu Pago se ha realizado satisfactoriamente, en unos segundos se reflejará en tu saldo");
+    		return view('pages.dashboard.credito');
+    	}	
+	}
 
-		// return $mensaje;
+    public function confirmacionPasarela(Request $request){
+    	//Validar firma
+		$signature = hash('sha256', $request->x_cust_id_cliente.'^86c18a3ad068b30d14c99a47940ad176bb0c7721^'.$request->x_ref_payco.'^'.$request->x_transaction_id.'^'.$request->x_amount.'^'.$request->x_currency_code);
+		
+		if ($signature == $request->x_signature) {
+			$codigoRespuesta = $request->x_cod_response;
+			if ($codigoRespuesta == 1) {
+				$saldoRecarga = $request->x_amount;
+				$usuario = User::findOrFail($request->x_extra1);
+				$usuario->saldo = $usuario->saldo + $saldoRecarga;
+				$usuario->save();
+
+				$mensaje ="Saldo cargado";
+			}else{
+				$mensaje ="No Realizada, estado = ".$codigoRespuesta;
+			}
+		}else{
+			$mensaje = "No coincide la firma";
+		}
+
+		return $mensaje;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,11 @@
 Route::post('publicaciones/detalle', 'PublicacionController@respuestaPasarela')->name('respuesta.publicacion');
 
 //Pagina de respuesta epay co agregar credito
-Route::post('credito/agregar', 'PublicacionController@respuestaPasarela')->name('respuesta.publicacion');
+Route::post('credito/agregar/respuesta', 'PublicacionController@respuestaPasarela')->name('respuesta.credito.agregar');
 
 //Pagina de confirmacion epay co publicacion o match
 Route::post('publicar/confirmacion', 'PublicacionController@confirmacionPasarela')->name('confirmacion.publicar');
 
 //Pagina de confirmacion epay co agregar credito
-Route::post('credito/agregar', 'CreditoController@confirmacionPasarela')->name('confirmacion.credito.agregar');
+Route::post('credito/agregar/confirmacion', 'CreditoController@confirmacionPasarela')->name('confirmacion.credito.agregar');
 

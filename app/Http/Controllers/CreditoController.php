50d3f3643b4861e73322de1151bac66541d77420
elkinff <elkin9309@gmail.com>
2018-02-23 17:42:22 -0500

    arreglo pagina confrimacin

--- app/Http/Controllers/CreditoController.php
+++ app/Http/Controllers/CreditoController.php
@@ -15,22 +15,22 @@ class CreditoController extends Controller{
     }
 
     public function confirmacionPasarela(){
-    	//Validar firma
-		$signature = hash('sha256', $request->x_cust_id_cliente.'^86c18a3ad068b30d14c99a47940ad176bb0c7721^'.$request->x_ref_payco.'^'.$request->x_transaction_id.'^'.$request->x_amount.'^'.$request->x_currency_code);
+  //   	//Validar firma
+		// $signature = hash('sha256', $request->x_cust_id_cliente.'^86c18a3ad068b30d14c99a47940ad176bb0c7721^'.$request->x_ref_payco.'^'.$request->x_transaction_id.'^'.$request->x_amount.'^'.$request->x_currency_code);
 		
-		if ($signature == $request->x_signature) {
-			$codigoRespuesta = $request->x_cod_response;
-			if ($codigoRespuesta == 1) {
-				$usuario = User::findOrFail($request->extra1);
-				// $usuario->saldo = $usuario->saldo + 
-			}
-			}else{
-				$mensaje ="No Realizada, estado = ".$codigoRespuesta;
-			}
-		}else{
-			$mensaje = "No coincide la firma";
-		}
+		// if ($signature == $request->x_signature) {
+		// 	$codigoRespuesta = $request->x_cod_response;
+		// 	if ($codigoRespuesta == 1) {
+		// 		$usuario = User::findOrFail($request->extra1);
+		// 		// $usuario->saldo = $usuario->saldo + 
+		// 	}
+		// 	}else{
+		// 		$mensaje ="No Realizada, estado = ".$codigoRespuesta;
+		// 	}
+		// }else{
+		// 	$mensaje = "No coincide la firma";
+		// }
 
-		return $mensaje;
+		// return $mensaje;
     }
 }

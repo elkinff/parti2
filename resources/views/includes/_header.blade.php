fc8b8f19f6fc4fa28f6b42cadd3cf553b6f66272
elkinff <elkin9309@gmail.com>
2018-02-23 15:20:44 -0500

    arreglo foto usuario logeado

--- resources/views/includes/_header.blade.php
+++ resources/views/includes/_header.blade.php
@@ -34,7 +34,7 @@
 				
 				<a id="dropdownUser" class="dropbtn header__menu__item header__menu__item--user">
 					@if(Auth::user()->foto)
-						<img src="/img/usuario/{{$usuario->foto}}" >
+						<img src="/img/usuario/{{Auth::user()->foto}}" >
 					@else 
 						<img src="{{ asset('img/email/users.png') }}" >
 					@endif

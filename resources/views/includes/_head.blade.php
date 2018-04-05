284aeedab55a96babec535ab0e955a44457f8010
OscarSoler <oscarsoler25@gmail.com>
2018-04-05 09:39:24 -0500

    Secure Assest head

--- resources/views/includes/_head.blade.php
+++ resources/views/includes/_head.blade.php
@@ -6,13 +6,13 @@
     <title>Parti2</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ secure_asset('css/app.css')}}">
 
     {{-- Favicon  --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
-	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
-	<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
-	<link rel="manifest" href="{{ asset('img/favicon/site.webmanifest') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ secure_asset('img/favicon/apple-touch-icon.png') }}">
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ secure_asset('img/favicon/favicon-32x32.png') }}">
+	<link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('img/favicon/favicon-16x16.png') }}">
+	<link rel="manifest" href="{{ secure_asset('img/favicon/site.webmanifest') }}">
 	<meta name="msapplication-TileColor" content="#355C7D">
 
 	{{-- Metas --}}
